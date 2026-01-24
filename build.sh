71411a1736b172a6991c4f3ac4b14b151b88529c
Krish Sutariya <krishsutariya98@gmail.com>
2026-01-24 08:47:12 +0530

    updates

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ date
 echo "Hello from Jenkins CI pipeline!"
 echo "Build completed successfully."
 echo "  build completed."
-echo "help neeee i am under the water"
+echo "help neeee i am under the water its  soooo deepppp"
